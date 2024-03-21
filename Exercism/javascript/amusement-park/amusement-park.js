6bf42c9e6e25b9f6bae12b727e53f6dc9b7aa2f8
Radosław Terelak <radoslaw.terelak@gmail.com>
2024-03-21 17:52:56 +0100

    Exercism - JS - Amusement Park

---
+++ Exercism/javascript/amusement-park/amusement-park.js
@@ -0,0 +1,74 @@
+/// <reference path="./global.d.ts" />
+// @ts-check
+
+/**
+ * Creates a new visitor.
+ *
+ * @param {string} name
+ * @param {number} age
+ * @param {string} ticketId
+ * @returns {Visitor} the visitor that was created
+ */
+export function createVisitor(name, age, ticketId) {
+	return { name, age, ticketId };
+}
+
+/**
+ * Revokes a ticket for a visitor.
+ *
+ * @param {Visitor} visitor the visitor with an active ticket
+ * @returns {Visitor} the visitor without a ticket
+ */
+export function revokeTicket(visitor) {
+	visitor.ticketId = null;
+
+	return visitor;
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function ticketStatus(tickets, ticketId) {
+	const ticketName = tickets[ticketId];
+
+	switch (ticketName) {
+		case undefined:
+			return "unknown ticket id";
+		case null:
+			return "not sold";
+		default:
+			return `sold to ${ticketName}`;
+	}
+}
+
+/**
+ * Determines the status a ticket has in the ticket tracking object
+ * and returns a simplified status message.
+ *
+ * @param {Record<string, string|null>} tickets
+ * @param {string} ticketId
+ * @returns {string} ticket status
+ */
+export function simpleTicketStatus(tickets, ticketId) {
+	const ticketName = tickets[ticketId];
+
+	if (ticketName === null || ticketName === undefined) {
+		return "invalid ticket !!!";
+	}
+
+	return ticketName;
+}
+
+/**
+ * Determines the version of the GTC that was signed by the visitor.
+ *
+ * @param {VisitorWithGtc} visitor
+ * @returns {string | undefined} version
+ */
+export function gtcVersion(visitor) {
+	return visitor.gtc?.version;
+}
