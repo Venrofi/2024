2cd02e2766c3c621eee4580dd9a15b6be8f5a7fc
Radosław Terelak <radoslaw.terelak@gmail.com>
2024-06-20 20:15:07 +0200

    Exercism - JS - Perfect Numbers

---
+++ Exercism/javascript/perfect-numbers/perfect-numbers.js
@@ -0,0 +1,16 @@
+export const classify = (number) => {
+	if (number <= 0) throw new Error("Classification is only possible for natural numbers.");
+
+	let aliquotSum = 0;
+
+	for (let i = 1; i <= number / 2; i++) {
+		if (number % i === 0) {
+			aliquotSum += i;
+		}
+	}
+
+	if (aliquotSum === number) return "perfect";
+	if (aliquotSum > number) return "abundant";
+
+	return "deficient";
+};
