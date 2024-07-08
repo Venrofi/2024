b9f8d53f8e45c0234fb344c944fe53f7f6705eff
Radosław Terelak <radoslaw.terelak@gmail.com>
2024-07-08 21:57:10 +0200

    Exercism - JS - OCR Numbers

---
+++ Exercism/javascript/ocr-numbers/ocr-numbers.js
@@ -0,0 +1,34 @@
+const NUMBER_PATTERNS = {
+	" _ | ||_|": "0",
+	"     |  |": "1",
+	" _  _||_ ": "2",
+	" _  _| _|": "3",
+	"   |_|  |": "4",
+	" _ |_  _|": "5",
+	" _ |_ |_|": "6",
+	" _   |  |": "7",
+	" _ |_||_|": "8",
+	" _ |_| _|": "9",
+};
+
+export const convert = (input) => {
+	const rows = input.split("\n");
+	let result = "";
+
+	for (let blockIndex = 0; blockIndex < rows.length; blockIndex += 4) {
+		if (blockIndex > 0) {
+			result += ",";
+		}
+
+		for (let column = 0; column < rows[0].length; column += 3) {
+			const digitPattern = rows
+				.slice(blockIndex, blockIndex + 3)
+				.map((row) => row.slice(column, column + 3))
+				.join("");
+
+			result += NUMBER_PATTERNS[digitPattern] || "?";
+		}
+	}
+
+	return result;
+};
