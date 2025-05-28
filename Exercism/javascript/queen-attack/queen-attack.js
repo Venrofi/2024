2f87b90c8146ca0b17a6807f9ebba3fc57f53c9e
Radosław Terelak <radoslaw.terelak@comarch.com>
2025-05-28 13:40:32 +0200

    Exercism - JS - Queen Attack v2

--- Exercism/javascript/queen-attack/queen-attack.js
+++ Exercism/javascript/queen-attack/queen-attack.js
@@ -1,24 +1,16 @@
 export class QueenAttack {
   constructor({
-    black: [blackRow, blackColumn] = [],
-    white: [whiteRow, whiteColumn] = [],
+    black: [blackRow, blackColumn] = [0, 3],
+    white: [whiteRow, whiteColumn] = [7, 3],
   } = {}) {
     this.white = [whiteRow, whiteColumn];
     this.black = [blackRow, blackColumn];
-
-    if (whiteRow === undefined && whiteColumn === undefined) {
-      this.white = [7, 3];
-    }
-
-    if (blackRow === undefined && blackColumn === undefined) {
-      this.black = [0, 3];
-    }
 
     if (!this.isPositionValid(this.white) || !this.isPositionValid(this.black)) {
       throw new Error('Queen must be placed on the board');
     }
 
-    if (this.white[0] === this.black[0] && this.white[1] === this.black[1]) {
+    if (whiteRow === blackRow && whiteColumn === blackColumn) {
       throw new Error('Queens cannot share the same space');
     }
   }
@@ -32,54 +24,14 @@ export class QueenAttack {
     board[whiteRow][whiteColumn] = 'W';
     board[blackRow][blackColumn] = 'B';
 
-    return board.map(r => r.join(' ')).join('\n');
+    return board.map(row => row.join(' ')).join('\n');
   }
 
   get canAttack() {
     const [whiteRow, whiteColumn] = this.white;
     const [blackRow, blackColumn] = this.black;
 
-    if (whiteRow === blackRow || whiteColumn === blackColumn) {
-      return true;
-    }
-
-    const directions = ['NW', 'NE', 'SW', 'SE'];
-
-    for (let direction of directions) {
-      let row = whiteRow;
-      let col = whiteColumn;
-
-      while (this.isPositionValid([row, col])) {
-        switch (direction) {
-          case 'NW':
-            row -= 1;
-            col -= 1;
-            break;
-          case 'SW':
-            row += 1;
-            col -= 1;
-            break;
-          case 'NE':
-            row -= 1;
-            col += 1;
-            break;
-          case 'SE':
-            row += 1;
-            col += 1;
-            break;
-          default:
-            row = -1;
-            col = -1;
-            break;
-        }
-
-        if (row === blackRow && col === blackColumn) {
-          return true;
-        }
-      }
-    }
-
-    return false;
+    return whiteRow === blackRow || whiteColumn === blackColumn || Math.abs(whiteRow - blackRow) === Math.abs(whiteColumn - blackColumn);
   }
 
   isPositionValid([row, column] = []) {
